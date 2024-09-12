b29b2c91f7c66f18a4dd607141c188f85ac5af2e
Jayden King <itsjaydenking@gmail.com>
2024-09-12 12:17:51 -0700

    Update Auth.js

--- src/Auth.js
+++ src/Auth.js
@@ -2,5 +2,5 @@ const CLIENT_ID = "c94106fb38c648399c3f429b9a931755"; // Your Spotify Client ID
 const REDIRECT_URI = "https://itsjaydenking.github.io/spotify-playlist-app/"; // GitHub Pages URL
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
-//${REDIRECT_URI}
-export const loginUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=https://itsjaydenking.github.io/spotify-playlist-app/&response_type=${RESPONSE_TYPE}&scope=playlist-modify-public`;
+
+export const loginUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=playlist-modify-public`;
