84f808b6c69a753c74c0c0044450464708ed5c46
Jayden King <itsjaydenking@gmail.com>
2024-09-12 12:03:03 -0700

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { loginUrl } from "./auth";
+import { loginUrl } from "./Auth";
 import "./App.css";
 
 function App() {
